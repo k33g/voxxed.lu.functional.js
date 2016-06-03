fba4ad197626949492ad5c5fe04ea94a0c1bb7a7
Philippe Charrière <k33g@github.com>
2016-06-03 19:26:22 +0200

    Update stools.js

--- stools.js
+++ stools.js
@@ -6,7 +6,23 @@ class Container {
     const value = x;
     Object.defineProperty(this, "value", { get: () => value }) // sortie
   }
+  
   static of(x) {
     return new Container(x);
+  }
+}
+
+class Functor {
+  constructor(x) {
+    const value = x;
+    Object.defineProperty(this, "value", { get: () => value }) // sortie
+  }
+  
+  static of(x) {
+    return new Functor(x);
+  }
+  
+  map (fn) {
+    return Functor.of(fn(this.value));
   }
 }
